9b963e24e05efafeb796671c6db28ec4ccb42ada
reggordon <reg@reggordon.com>
2015-09-02 14:05:05 +0000

    template for website

---
+++ app/assets/javascripts/external.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$('.zoom-gallery').magnificPopup
+	({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+'" target="_blank"></a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
